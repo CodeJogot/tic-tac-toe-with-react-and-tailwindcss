6222ecf32bfabc785e5f27ecfe92ccf9e2adeea7
FarmerAbdulAlim <farmerabdulalim@gmail.com>
2025-10-15 11:36:14 +0600

    project done

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,16 +1,42 @@
-import { useState } from "react";
+
 import Square from "./Square";
 
 
-export default function Board() {
-  const [squares, setSquares] = useState([]);
+export default function Board({xIsNext, squares, onPlay}) {
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) status = 'Winner: ' + winner;
+  else status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+
   function handleClick(i) {
+    if (squares[i] || winner) return;
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
-    setSquares(nextSquares);
+    if (xIsNext) nextSquares[i] = 'X';
+    else nextSquares[i] = 'O';
+    onPlay(nextSquares);
+  }
+  function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
   }
   return (
     <>
+        <div>{status}</div>
         <div className="flex">
           <Square value={squares[0]} onSquareClick = {() => handleClick(0)}/>
           <Square value={squares[1]} onSquareClick = {() => handleClick(1)}/>
